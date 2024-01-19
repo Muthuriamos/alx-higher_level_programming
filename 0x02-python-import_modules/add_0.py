f6410e668a5506cc3a27015580b03e26c31a10a9
muthuriamos <amosmuthuuri@gmail.com>
2024-01-19 08:54:12 +0300

    a program that imports the function def add(a, b):

--- 0x02-python-import_modules/add_0.py
+++ 0x02-python-import_modules/add_0.py
@@ -2,10 +2,10 @@ dd(a, b):
     """My addition function
 
     Args:
-    a: first integer
-    b: second integer
+        a: first integer
+        b: second integer
 
     Returns:
         The return value. a + b
-        """
+    """
         return (a + b)
