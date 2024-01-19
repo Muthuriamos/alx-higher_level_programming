0c0cca216e21eb90406b18795774d0635da4da8c
muthuriamos <amosmuthuuri@gmail.com>
2024-01-19 11:11:06 +0300

    a program that prints all the names defined by the compiled module

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__name__":
-from hidden_4 import *
+    from hidden_4 import *
     allf = dir()
     for i in range(0, len(allf)):
         if allf[i][;2] !+ "__":
